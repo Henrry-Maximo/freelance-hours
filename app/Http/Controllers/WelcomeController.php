ffef8d63bd7a0e111ce74403820588c65296a518
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-10-11 11:10:23 -0300

    ✨Feat: Add Diretrizes Blade and Controller User

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -11,6 +12,27 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $user = User::find(1);
+        // $user = User::query()->create([
+        //     "name"=> 'Rafael',
+        //     'email'=> 'rafael@email.com',
+        //     'password' => bcrypt('@123456'),
+        // ]);
+
+        // var_dump($user);
+        // dump($user);
+        // die();
+        // dd($user);
+
+        // Options 1 and 2
+        $user->email_verified_at = now();
+        $user->save();
+
+        $user->update(['email_verified_at' => now()]);
+
+        dd($user->email_verified_at);
+        
+
         return view('teste.jeremias');
     }
 }
--- resources/views/teste/jeremias.blade.php
+++ resources/views/teste/jeremias.blade.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Rocketseat PHP</title>
 </head>
+
 <body>
-  <h1>Olá, meu lindo!</h1>
+  <!-- PHP Puro -->
+  <!-- <h1>Olá, meu lindo!</h1> -->
+  <?php echo 'oi Jeremias' ?>
+  <?php foreach (range(1, 20) as $key): ?>
+  <li><?php  echo $key; ?></li>
+  <?php endforeach; ?>
+
+  <!-- Diretiva Blade @auth -->
+  @auth
+    <span>Você está autenticado</span>
+  @endauth
+
+   <!-- Diretiva @foreach do Blade -->
+  @foreach(range(1, 20) as $key)
+    <li>{{ $key }}</li>
+  @endforeach
 </body>
+
 </html>
